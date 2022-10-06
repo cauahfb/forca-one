d97a85c2f7e28fc4644ca77f6f612dbb1dd8360f
caua_hfb <cauaferreirabarbosa@hotmail.com>
2022-10-06 17:11:42 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+let palavras = ["ALURA","ORACLE","FORCA","HTML","JAVASCRIPT","LOGICA"];
+let tabuleiro = document.getElementById("forca").getContext("2d");
+let palavraSecreta = "";
+
+function escolherPalavraSecreta(){
+    let palavra = palavras[Math.floor(Math.random()*palavras.length)]
+    palavraSecreta = palavra;
+    console.log(palavraSecreta)
+}
+
+function iniciarJogo(){
+    document.getElementById("div-desaparece").style.display = "none"
+    escolherPalavraSecreta();
+    desenharCanvas();
+    desenharLinhas();
+}
