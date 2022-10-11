614a9373f8b8c91d282fa11c6861658a0cf1037b
Lucas <lucaslimavzt@gmail.com>
2022-10-11 10:13:25 -0300

    lucas alteration

--- script.js
+++ script.js
@@ -1,62 +1,64 @@
-let palavras = ["ALURA","ORACLE","FORCA","HTML","JAVASCRIPT","LOGICA"];
+let palavras = ["ALURA", "ORACLE", "FORCA", "HTML", "JAVASCRIPT", "LOGICA"];
 let tabuleiro = document.getElementById("forca").getContext("2d");
 let palavraSecreta = "";
 
 let letras = [];
 let erros = 8;
 
-function escolherPalavraSecreta(){
-    let palavra = palavras[Math.floor(Math.random()*palavras.length)]
+function escolherPalavraSecreta() {
+    let palavra = palavras[Math.floor(Math.random() * palavras.length)]
     palavraSecreta = palavra;
     console.log(palavraSecreta)
     return palavra
 }
 
-function verificarLetra(key){
-    let estado = false
-    if(key >= 65 && letras.indexOf(key) || key <= 90 && letras.indexOf(key)){
-        letras.push(key);
-        console.log(key)
-        console.log(letras, + "if false")
-        return estado
-    }
-    else{
-        estado = true;
-        letras.push(key);
-        console.log(key)
-        console.log(letras, + "if true")
-        return estado
-    }
+function verificarTecla(tecla) {
+    if (tecla.length > 1) return false;
+    let key = tecla.charCodeAt(0);
 
+    if (key >= 65 && key <= 90) {
+        return true;
+    }
+    return false;
 }
 
-function adicionarLetraIncorreta(){
+function adicionarLetraIncorreta() {
     erros -= 1
 }
 
+function letraNaoRapetida(letra) {
+    return !letras.includes(letra);
+}
 
-function iniciarJogo(){
+
+function iniciarJogo() {
     document.getElementById("div-desaparece").style.display = "none"
     escolherPalavraSecreta();
-    
+
     desenharCanvas();
     desenharLinhas();
 
-    document.onkeydown = (e) =>{
+    document.onkeydown = (e) => {
 
-        let letra = e.key.toUpperCase()
+        let letra = e.key.toUpperCase();
 
-        if(verificarLetra(letra) && palavraSecreta.includes(letra)){
-            for(let i = 0; i<palavraSecreta.length; i++){
-                if(palavraSecreta[i] === letra){
-                    escreverLetraCorreta(i)
+        if (verificarTecla(letra) && letraNaoRapetida(letra)) {
+            if (palavraSecreta.includes(letra)) {
+                for (let i = 0; i < palavraSecreta.length; i++) {
+                    if (palavraSecreta[i] === letra) {
+                        escreverLetraCorreta(i);
+                    }
                 }
-            }            
 
-        }
-        else{
-            adicionarLetraIncorreta(letra)
-            escreverLetraIncorreta(letra,erros)
+            }
+            else {
+                adicionarLetraIncorreta(letra);
+                escreverLetraIncorreta(letra, erros);
+            }
+            letras.push(letra);
+
+        } else {
+            console.log("nao entrou");
         }
     }
 }
